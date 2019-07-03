67c66ef4ed8c15cd085349aead745db5a6193372
jerry-drj <rjdingc@isoftstone.com>
2019-07-03 22:58:30 +0800

    Signed-off-by: jerry-drj <rjdingc@isoftstone.com>vue-ts项目探索

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<!--<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>-->
+
+<!--<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>-->
+
+<template>
+  <div>
+    <input v-model="msg">
+    <p>msg: {{ msg }}</p>
+    <p>computed msg: {{ computedMsg }}</p>
+    <button @click="greet">Greet</button>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+  import HelloWorld from './components/HelloWorld.vue'
+
+  @Component({
+    components: {
+      HelloWorld
+    }
+  })
+  class App extends Vue {
+    // 初始化数据
+    msg = 123
+
+    // 声明周期钩子
+    mounted () {
+      this.greet()
+    }
+
+    // 计算属性
+    get computedMsg () {
+      return 'computed ' + this.msg
+    }
+
+    // 方法
+    greet () {
+      alert('greeting: ' + this.msg)
+    }
+  }
+  export default App
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
