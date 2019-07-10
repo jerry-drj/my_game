947a693934a757513296b62ca5e72aaa95370d15
jerry-drj <rjdingc@isoftstone.com>
2019-07-10 21:27:42 +0800

    nodeServe初步搭建

---
+++ api/fetch.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+let fetch = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+  });
+
+  //添加请求拦截器
+  fetch.interceptors.request.use(config =>{
+  //在发送请求之前做某事
+  return config;
+},(error) =>{
+  //请求错误时做些事
+  return Promise.reject(error);
+});
+
+//添加响应拦截器
+fetch.interceptors.response.use(response => {
+  //对响应数据做些事
+  return response;
+},(error) =>{
+  //请求错误时做些事
+  return Promise.reject(error);
+});
+module.exports =fetch;
---
+++ routers/www/index.js
@@ -0,0 +1,87 @@
+const Router=require('koa-router');
+
+let router=new Router();
+
+router.get('', async ctx=>{
+  let {HTTP_ROOT}=ctx.config;
+  console.log(ctx)
+
+  let banners=await ctx.db.query("SELECT * FROM banner_table ORDER BY serial ASC");
+  let catalogs=await ctx.db.query("SELECT * FROM catalog_table");
+  let articles=await ctx.db.query(`
+    SELECT
+    *,
+    article_table.title AS article_title,
+    catalog_table.title AS catalog_title,
+    article_table.ID AS article_ID
+    FROM article_table
+    LEFT JOIN catalog_table ON article_table.catalog_ID=catalog_table.ID
+    ORDER BY article_table.created_time DESC LIMIT 10
+  `);
+
+  articles.forEach(article=>{
+    let oDate=new Date(article.created_time*1000);
+
+    article.created_time=`${oDate.getFullYear()}-${oDate.getMonth()+1}-${oDate.getDate()}`;
+  });
+
+
+  // await ctx.render('www/index', {
+  //   HTTP_ROOT,
+  //   banners,
+  //   catalogs,
+  //   articles
+  // });
+  ctx.body = 'index'
+});
+
+router.get('list/:id/', async ctx=>{
+  let {id}=ctx.params;
+  let {HTTP_ROOT}=ctx.config;
+
+  let rows=await ctx.db.query('SELECT * FROM catalog_table WHERE ID=?', [id]);
+  let articles=await ctx.db.query(`
+    SELECT
+    *,
+    article_table.title AS article_title,
+    catalog_table.title AS catalog_title,
+    article_table.ID AS article_ID
+    FROM article_table
+    LEFT JOIN catalog_table ON article_table.catalog_ID=catalog_table.ID
+    WHERE article_table.catalog_ID=${id}
+    ORDER BY article_table.created_time DESC LIMIT 10
+  `);
+
+  articles.forEach(article=>{
+    let oDate=new Date(article.created_time*1000);
+
+    article.created_time=`${oDate.getFullYear()}-${oDate.getMonth()+1}-${oDate.getDate()}`;
+  });
+
+  // await ctx.render('www/list', {
+  //   HTTP_ROOT,
+  //   catalog_title: rows[0].title,
+  //   articles
+  // });
+  ctx.body = 'list'
+});
+
+router.get('article/:id/', async ctx=>{
+  let {id}=ctx.params;
+  let {HTTP_ROOT}=ctx.config;
+
+  let rows=await ctx.db.query("SELECT * FROM article_table WHERE ID=?", [id]);
+  let article=rows[0];
+
+  let oDate=new Date(article.created_time*1000);
+
+  article.created_time=`${oDate.getFullYear()}-${oDate.getMonth()+1}-${oDate.getDate()}`;
+
+  // await ctx.render('www/article', {
+  //   HTTP_ROOT,
+  //   article
+  // });
+  ctx.body = 'article'
+});
+
+module.exports=router.routes();
