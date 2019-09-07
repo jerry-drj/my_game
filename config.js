27b3869fc2fcda5d2549605cc7bde198afff8016
dingruijie001 <dingruijie001@gmail.com>
2019-09-07 16:34:06 +0800

    heroku init

---
+++ config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    DB_HOST: 'localhost',
+    DB_USER: 'root',
+    DB_PASS: 'root',
+    DB_NAME: 'cpts',
+
+    PORT: 3000,
+
+    ADMIN_PREFIX: '_?:L$"OPUIOSIFJ(*UPT:LKRFG',
+
+    HTTP_ROOT: 'http://localhost:3000',
+    //HTTP_ROOT: 'https://www.aaa.com',
+
+    UPLOAD_DIR: path.resolve(__dirname, './static/upload'),
+};
